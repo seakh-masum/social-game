1dfdb1deefc874e2405902fd71b9e28ba2e88660
sayon1998 <sayonchakraborty1998@gmail.com>
2021-03-14 22:48:30 +0530

    Setup Admin Panel

---
+++ admin/src/app/Shared/core.module.ts
@@ -0,0 +1,68 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatExpansionModule,
+    ClipboardModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+  ],
+  exports: [
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatExpansionModule,
+    ClipboardModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+  ],
+})
+export class CoreModule {}
